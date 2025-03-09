03351dd3a97c0372f5aaff0e6b16ad55de489a43
Md. Sifat Kamal <md.sifat.kamal@g.bracu.ac.bd>
2025-03-10 03:56:28 +0600

    structured files

---
+++ booking/booking.py
@@ -0,0 +1,42 @@
+import os
+import booking.constants as const
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.select import Select
+from selenium.common.exceptions import NoSuchElementException
+from openpyxl import load_workbook
+from datetime import datetime
+import time
+
+class Booking:
+
+    def __init__(self, driver_path = r"C:/SeleniumDrivers", teardown = False):
+
+        self.driver_path = driver_path
+        self.teardown = teardown
+
+        os.environ['PATH'] += self.driver_path
+
+        options = webdriver.ChromeOptions()
+        options.add_experimental_option("detach", True)
+        options.add_experimental_option('excludeSwitches', ['enable-logging'])
+        self.driver = webdriver.Chrome(options=options)
+        self.driver.implicitly_wait(15)
+        self.driver.maximize_window()
+
+    def __enter__(self):
+
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        
+        if self.teardown == True:
+            
+            self.driver.quit()
+
+    def land_page(self):
+
+        self.driver.get(const.BASE_URL)
+        
